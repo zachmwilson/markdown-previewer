dbc0ef6b80b2836fcc2e4b893193883ff4814f5a
Zachary Wilson <goodzach@gmail.com>
2025-02-24 21:36:18 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+
+import Editor from "./components/Editor";
+import Previewer from "./components/Previewer";
+import { initialText } from "./markdown";
+
 import "./App.css";
-import ReactMarkdown from "react-markdown";
 
-// defaultMarkdown contains valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text
-const defaultMarkdown = `
-# Welcome to my React Markdown Previewer!
-## This is a sub-heading...
-### And here's some other cool stuff:
+const AppBox = styled.div`
+  width: 100%;
+  height: var(--app-height);
+  padding: 1.2rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.2rem;
+  overflow: auto;
+`;
 
-Heres some code, \`<div></div>\`, between 2 backticks.
+const App = () => {
+  const [editorFullSize, setEditorFullSize] = useState(false);
+  const handleEditorFullSize = () => setEditorFullSize((prev) => !prev);
+  const [previewFullSize, setPreviewFullSize] = useState(false);
+  const handlePreviewFullSize = () => setPreviewFullSize((prev) => !prev);
 
-\`\`\`
-// this is multi-line code:
+  const [text, setText] = useState(initialText);
 
-function anotherExample(firstLine, lastLine) {
-  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
-    return multiLineCode;
-  }
-}
-\`\`\`
+  const setAppHeight = () => {
+    const doc = document.documentElement;
+    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
+  };
 
-You can also make text **bold**... whoa!
-Or _italic_. Or... wait for it... **_both!_**
-And feel free to go crazy ~~crossing stuff out~~.
-
-There's also [links](https://www.freecodecamp.com), and
-> Block Quotes!
-
-And if you want to get really crazy, even tables:
-
-| Wild Header | Crazy Header | Another Header? |
-| ------------ | ------------ | --------------- |
-| Your content can | be here, and it | can be here.... |
-| And here. | Okay. | I think we get it.
-
-- And of course there are lists.
-  - Some are bulleted.
-     - With different indentation levels.
-        - That look like this.
-
-1. And there are numbered lists too.
-1. Use just 1s if you want!
-1. But the list goes on...
-
-- Even if you use dashes or asterisks.
-
-And last but not least, let's not forget embedded images:
-![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg)`;
-
-function App() {
-  const [markdownText, setMarkdownText] = useState<string>("defaultMarkdown");
+  useEffect(() => {
+    window.addEventListener("resize", setAppHeight);
+    setAppHeight();
+    return () => window.removeEventListener("resize", setAppHeight);
+  }, []);
 
   return (
-    <>
-      <div>
-        <h1 style={{ textAlign: "center" }}>Markdown Previewer</h1>
-        <div className="boxes-container">
-          <textarea
-            name="editor"
-            id="editor"
-            value={markdownText}
-            onChange={(e) => setMarkdownText(e.target.value)}
-          ></textarea>
-          <div id="preview">
-            <ReactMarkdown>{markdownText}</ReactMarkdown>
-          </div>
-        </div>
-      </div>
-    </>
+    <main>
+      <AppBox>
+        <Editor
+          editorFullSize={editorFullSize}
+          previewFullSize={previewFullSize}
+          handleEditorFullSize={handleEditorFullSize}
+          text={text}
+          setText={setText}
+        />
+        <Previewer
+          editorFullSize={editorFullSize}
+          previewFullSize={previewFullSize}
+          handlePreviewFullSize={handlePreviewFullSize}
+          text={text}
+        />
+      </AppBox>
+    </main>
   );
-}
-
+};
 
 export default App;
