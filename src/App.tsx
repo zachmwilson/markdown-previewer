929c35ac8e9262271653822025e21935fb3ad971
Zachary Wilson <goodzach@gmail.com>
2025-02-24 21:47:06 -0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,62 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-
-import Editor from "./components/Editor";
-import Previewer from "./components/Previewer";
-import { initialText } from "./markdown";
-
+import { useState } from "react";
 import "./App.css";
+import ReactMarkdown from "react-markdown";
 
-const AppBox = styled.div`
-  width: 100%;
-  height: var(--app-height);
-  padding: 1.2rem;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 1.2rem;
-  overflow: auto;
+// defaultMarkdown contains valid markdown that represents at least one of each of the following elements: a header (H1 size), a sub header (H2 size), a link, inline code, a code block, a list item, a blockquote, an image, and bolded text
+const defaultMarkdown = `
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... **_both!_**
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+![React Logo w/ Text](https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png)
+
+- And of course there are lists.
+  - Some are bulleted.
+      - With different indentation levels.
+        - That look like this.
 `;
 
-const App = () => {
-  const [editorFullSize, setEditorFullSize] = useState(false);
-  const handleEditorFullSize = () => setEditorFullSize((prev) => !prev);
-  const [previewFullSize, setPreviewFullSize] = useState(false);
-  const handlePreviewFullSize = () => setPreviewFullSize((prev) => !prev);
-
-  const [text, setText] = useState(initialText);
-
-  const setAppHeight = () => {
-    const doc = document.documentElement;
-    doc.style.setProperty("--app-height", `${window.innerHeight}px`);
-  };
-
-  useEffect(() => {
-    window.addEventListener("resize", setAppHeight);
-    setAppHeight();
-    return () => window.removeEventListener("resize", setAppHeight);
-  }, []);
+function App() {
+  const [markdownText, setMarkdownText] = useState<string>(defaultMarkdown);
 
   return (
-    <main>
-      <AppBox>
-        <Editor
-          editorFullSize={editorFullSize}
-          previewFullSize={previewFullSize}
-          handleEditorFullSize={handleEditorFullSize}
-          text={text}
-          setText={setText}
-        />
-        <Previewer
-          editorFullSize={editorFullSize}
-          previewFullSize={previewFullSize}
-          handlePreviewFullSize={handlePreviewFullSize}
-          text={text}
-        />
-      </AppBox>
-    </main>
+    <>
+      <div>
+        <h1 style={{ textAlign: "center" }}>Markdown Previewer</h1>
+        <div className="boxes-container">
+          <textarea
+            name="editor"
+            id="editor"
+            value={markdownText}
+            onChange={(e) => setMarkdownText(e.target.value)}
+          ></textarea>
+          <div id="preview">
+            <ReactMarkdown>{markdownText}</ReactMarkdown>
+          </div>
+        </div>
+      </div>
+    </>
   );
-};
+}
 
 export default App;
